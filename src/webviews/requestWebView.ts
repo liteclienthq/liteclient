7a3fcd3916cd27aebedbe28224c5508847f747b2
Vikas Sharma <master2vikas@gmail.com>
2025-12-20 04:21:27 +0530

    fix: move webview template to media to include in package

--- src/webviews/requestWebView.ts
+++ src/webviews/requestWebView.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 export class RequestWebView {
   static getHtmlContent(context: vscode.ExtensionContext): string {
     // Read the HTML template file
-    const templatePath = path.join(context.extensionPath, 'src', 'webviews', 'requestWebViewTemplate.html');
+    const templatePath = path.join(context.extensionPath, 'media', 'requestWebViewTemplate.html');
     return fs.readFileSync(templatePath, 'utf8');
   }
 }
